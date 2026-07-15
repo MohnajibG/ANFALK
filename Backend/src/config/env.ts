2b56ad8ca6d81d222628c898e4407e30845d0a77
GUERCHAOUI <guerchaoui7@hotmail.com>
2026-07-16 01:02:34 +0200

    fix server

--- Backend/src/config/env.ts
+++ Backend/src/config/env.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const env = {
-  PORT: Number(process.env.PORT),
+  PORT: Number(process.env.PORT) || 3000,
 
   MONGO_URI: process.env.MONGO_URI || "",
 
